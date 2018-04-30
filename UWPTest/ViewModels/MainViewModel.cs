e7399501cb5f8e18b31544f02aa0f25924176156
pandasa <pandasa@umich.edu>
2018-04-30 01:46:13 -0400

    Add project files.

---
+++ UWPTest/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using UWPTest.Helpers;
+
+namespace UWPTest.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ UWPTest/Views/ShellPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using UWPTest.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace UWPTest.Views
+{
+    public sealed partial class ShellPage : Page
+    {
+        public ShellViewModel ViewModel { get; } = new ShellViewModel();
+
+        public ShellPage()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+            ViewModel.Initialize(shellFrame);
+        }
+    }
+}
